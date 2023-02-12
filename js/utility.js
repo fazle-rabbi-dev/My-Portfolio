c1626389b511580869cbcf394e767f8d60a1ab0f
fh-rabbi <frabby983@gmail.com>
2023-02-12 20:45:55 +0600

    Updated

--- js/utility.js
+++ js/utility.js
@@ -1,6 +1,6 @@
 const backToTopBtn = document.querySelector(".back-to-top");
 
-
+// Back To Top
 window.addEventListener("scroll", ()=>{
 	if(window.pageYOffset > 120){
 		backToTopBtn.style.opacity = 1;
@@ -15,3 +15,18 @@ window.addEventListener("scroll", ()=>{
 backToTopBtn.addEventListener("click", ()=>{
 	window.scrollTo(0,0);
 });
+
+
+// Dark/Light Mode
+const modeBtn = document.querySelector(".mode");
+
+
+function toggleDarkMode(label){
+	
+}
+
+modeBtn.addEventListener("click", ()=>{
+	// document.body.style.background = '#b6b6b6'
+	alert("Dark-Mode is not available right now.")
+});
+
