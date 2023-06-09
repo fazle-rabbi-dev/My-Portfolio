df6f0a89ae735e8bb0eadbbfcfcf66d33115011a
fh-rabbi <frabby983@gmail.com>
2023-06-09 22:31:42 +0600

    Updated ui

--- js/navbar.js
+++ js/navbar.js
@@ -1,21 +1,22 @@
 const open_menu_btn = document.querySelector(".nav-menu");
-const nav_bar = document.querySelector(".nav-bar");
+const side_bar = document.querySelector(".nav-bar");
+const nav_bar = document.querySelector(".nav-container");
 const close_menu_btn = document.querySelector(".close-nav");
 const header = document.querySelector("header");
 
 
 function toggleNavBar(label){
 	if(label == 'open'){
-		if(nav_bar.classList.contains('close')){
-			nav_bar.classList.remove('close');
+		if(side_bar.classList.contains('close')){
+			side_bar.classList.remove('close');
 		}
-		nav_bar.classList.add(label);
+		side_bar.classList.add(label);
 	}
 	else{
-		if(nav_bar.classList.contains('open')){
-			nav_bar.classList.remove('open');
+		if(side_bar.classList.contains('open')){
+			side_bar.classList.remove('open');
 		}
-		nav_bar.classList.add(label);
+		side_bar.classList.add(label);
 	}
 }
 
@@ -33,10 +34,10 @@ close_menu_btn.addEventListener("click", ()=>{
 
 function hideHeader(label){
 	if(label == 'hide'){
-		header.style.opacity = '0';
+		nav_bar.style.opacity = '0';
 	}
 	else{
-		header.style.opacity = '1';
+		nav_bar.style.opacity = '1';
 	}
 }
 
@@ -46,7 +47,7 @@ window.addEventListener("scroll", ()=>{
    let current_scroll_top = window.pageYOffset;
 	if(last_scroll_top < current_scroll_top){
 		hideHeader('hide');
-		toggleNavBar('close');
+		// toggleNavBar('close');
 	}else{
 		hideHeader('show');
 	}
--- js/utility.js
+++ js/utility.js
@@ -5,11 +5,9 @@ window.addEventListener("scroll", ()=>{
 	if(window.pageYOffset > 120){
 		backToTopBtn.style.opacity = 1;
 	}
-	
 	if(window.pageYOffset < 120){
 		backToTopBtn.style.opacity = 0;
 	}
-	
 });
 
 backToTopBtn.addEventListener("click", ()=>{
@@ -17,13 +15,19 @@ backToTopBtn.addEventListener("click", ()=>{
 });
 
 
+// Go to specific section
+function goToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+    document.querySelector(".nav-bar")
+      .classList.remove('open');
+  }
+}
+
+
 // Dark/Light Mode
 const modeBtn = document.querySelector(".mode");
-
-
-function toggleDarkMode(label){
-	
-}
 
 
 //
@@ -48,15 +52,31 @@ function showToast(mode){
 }
 
 const body = document.body;
+const navContainer = document.querySelector('.nav-container');
 const intro = document.querySelector('.intro');
 const aboutText = document.querySelector('#about-text');
 const sectionLabelh3 = document.querySelectorAll('.section-label h3');
 const sectionLabelhr = document.querySelectorAll('.section-label > hr');
-const socialIcons = document.querySelectorAll('.fab');
+const socialIcons = document.querySelectorAll('.socialicon');
+const projectsCard = document.querySelectorAll('.card');
+const skillCard = document.querySelectorAll('.skill');
+const contactForm = document.querySelector('.contact-container form');
 
   
+let isSystemInDarkMode = isDarkModeEnabled()
 let isDarkMode = false
-modeBtn.addEventListener("click", ()=>{
+
+if(isSystemInDarkMode){
+  // isDarkMode = true
+  toggleDarkMode()
+}
+
+modeBtn.addEventListener("click", ()=> {
+  // alert(isDarkMode)
+  toggleDarkMode()
+})
+
+function toggleDarkMode(){
   if(isDarkMode){
     // remove dark mode class
     body.classList.remove('bg-dark')
@@ -72,12 +92,19 @@ modeBtn.addEventListener("click", ()=>{
     socialIcons.forEach(el=>{
       el.classList.remove('text-light')
     })
+    projectsCard.forEach(el=>{
+      el.classList.remove('bg-dark-projects')
+    })
+    skillCard.forEach(el=>{
+      el.classList.remove('bg-dark-skills')
+    })
+    contactForm.classList.remove('bg-dark-contact')
     // Add light mode class
     body.classList.add('bg-light')
     intro.classList.add('text-dark')
     aboutText.classList.add('text-dark')
     sectionLabelh3.forEach(el=>{
-      el.classList.add('bg-dark')
+      el.classList.add('bg-dark-label')
       el.classList.add('text-light')
     })
     sectionLabelhr.forEach(el=>{
@@ -86,6 +113,13 @@ modeBtn.addEventListener("click", ()=>{
     socialIcons.forEach(el=>{
       el.classList.add('text-dark')
     })
+    projectsCard.forEach(el=>{
+      el.classList.add('bg-light-projects')
+    })
+    skillCard.forEach(el=>{
+      el.classList.add('bg-light-skills')
+    })
+    contactForm.classList.add('bg-light-contact')
     isDarkMode = false
     showToast('light')
     return;
@@ -99,7 +133,7 @@ modeBtn.addEventListener("click", ()=>{
   intro.classList.remove('text-dark')
   aboutText.classList.remove('text-dark')
   sectionLabelh3.forEach(el=>{
-    el.classList.remove('bg-dark')
+    el.classList.remove('bg-dark-label')
     el.classList.remove('text-light')
   })
   sectionLabelhr.forEach(el=>{
@@ -108,8 +142,16 @@ modeBtn.addEventListener("click", ()=>{
   socialIcons.forEach(el=>{
     el.classList.remove('text-dark')
   })
+  projectsCard.forEach(el=>{
+    el.classList.remove('bg-light-projects')
+  })
+  skillCard.forEach(el=>{
+    el.classList.remove('bg-light-skills')
+  })
+  contactForm.classList.remove('bg-light-contact')
   // Add dark mode class
   body.classList.add('bg-dark')
+  // navContainer.style.background = '#222'
   intro.classList.add('text-light')
   aboutText.classList.add('text-light')
   sectionLabelh3.forEach(el=>{
@@ -122,7 +164,23 @@ modeBtn.addEventListener("click", ()=>{
   socialIcons.forEach(el=>{
     el.classList.add('text-light')
   })
+  projectsCard.forEach(el=>{
+    el.classList.add('bg-dark-projects')
+  })
+  skillCard.forEach(el=>{
+    el.classList.add('bg-dark-skills')
+  })
+  contactForm.classList.add('bg-dark-contact')
   // 
   showToast('dark')
-});
+};
 
+
+// 
+function isDarkModeEnabled() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return true; // Dark mode is enabled
+  } else {
+    return false; // Dark mode is not enabled
+  }
+}
